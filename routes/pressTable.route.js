c1c5d5ded07b4df42821ce5d24709a487373e718
Sachin Rawat <logistics@qurvii.com>
2025-11-09 13:43:35 +0530

    return log routes added

--- routes/pressTable.route.js
+++ routes/pressTable.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { getPressTableRecords, createPressTableRecord, deletePressTableRecord } = require("../controllers/pressTable.controller");
+const { getPressTableRecords, createPressTableRecord, deletePressTableRecord, bulkDeleteRecordsFromPressTable } = require("../controllers/pressTable.controller");
 const router = express.Router();
 
 router.route("/get-records").get(getPressTableRecords);
 router.route("/add-record").post(createPressTableRecord);
 router.route("/delete-record").delete(deletePressTableRecord);
+router.route("/bulk_delete").delete(bulkDeleteRecordsFromPressTable);
 
 module.exports = router;
