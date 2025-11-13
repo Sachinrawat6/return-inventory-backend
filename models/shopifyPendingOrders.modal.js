965ac02710e3209a88efad8fa6f653be6447d52a
Sachin Rawat <logistics@qurvii.com>
2025-11-13 16:15:54 +0530

    unique removed

--- models/shopifyPendingOrders.modal.js
+++ models/shopifyPendingOrders.modal.js
@@ -4,7 +4,6 @@ const pendingOrderSchema = new mongoose.Schema({
   order_id: {
     type: String,
     required: true,
-    unique: true
   },
   styleNumber: { // ✅ fixed typo here
     type: Number,
